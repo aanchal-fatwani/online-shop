92bf9c3323b33675b6318d64c92df2815028854f
Aanchal Fatwani <aanchal.fatwani@impetus.com>
2023-07-10 22:17:59 +0530

    Added header app

---
+++ header/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="header">
+      <h1 className="main-heading">
+        eDUKAAN
+      </h1>
+    </div>
+  );
+};
+
+export default App;
