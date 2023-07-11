baa31a86d0d730fa7d86b804c7bfbba669b4d4b3
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-07-11 10:29:59 +0530

    Added header tabs

--- header/src/App.js
+++ header/src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./index.css";
 
 const App = () => {
+  let navigate = useNavigate();
+
   return (
     <div className="header">
-      <h1 className="main-heading">
+      <h1 className="main-heading" onClick={() => navigate("/")}>
         eDUKAAN
       </h1>
       <div style={{ right: "7px" }} className="header-section">
         {
           <>
-            <button className="header-tab"> Help </button>
-            <button className="header-tab"> Contact Us </button>
+            <button
+              className="header-tab"
+              onClick={() => navigate("/help")}
+            >
+              Help
+            </button>
+            <button
+              className="header-tab"
+              onClick={() => navigate("/contact-us")}
+            >
+              Contact Us
+            </button>
           </>
         }
       </div>
