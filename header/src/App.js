5e6975264a4079cefeec2dc41ec1613615a6b785
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-07-18 18:19:12 +0530

    Added shared routing to header

--- header/src/App.js
+++ header/src/App.js
@@ -2,26 +2,26 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./index.css";
 
-const App = () => {
-  let navigate = useNavigate();
+const App = ({ navigate }) => {
+  let customNavigate = navigate;
+  if (!navigate) {
+    customNavigate = useNavigate();
+  }
 
   return (
     <div className="header">
-      <h1 className="main-heading" onClick={() => navigate("/")}>
+      <h1 className="main-heading" onClick={() => customNavigate("/")}>
         eDUKAAN
       </h1>
       <div style={{ right: "7px" }} className="header-section">
         {
           <>
-            <button
-              className="header-tab"
-              onClick={() => navigate("/help")}
-            >
+            <button className="header-tab" onClick={() => customNavigate("/help")}>
               Help
             </button>
             <button
               className="header-tab"
-              onClick={() => navigate("/contact-us")}
+              onClick={() => customNavigate("/contact-us")}
             >
               Contact Us
             </button>
