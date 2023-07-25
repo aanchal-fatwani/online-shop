80c404b7fa73586874803237c21d5f771d72235c
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-07-25 23:19:29 +0530

    Added products API for category

--- dashboard/src/apis/Categories.js
+++ dashboard/src/apis/Categories.js
@@ -1,5 +1,11 @@
 export function getAllCategories() {
     return fetch('https://dummyjson.com/products/categories')
         .then(res => res.json())
-        // .then(console.log);
+    // .then(console.log);
+}
+
+export function getCategoryProducts(category) {
+    fetch(`https://dummyjson.com/products/category/${category}`)
+        .then(res => res.json())
+        .then(console.log);
 }
