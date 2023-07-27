4968dd69f89e4a577e202490dc5240db2438faab
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-07-27 23:49:10 +0530

    Added products on dashboard

--- dashboard/src/views/Dashboard.js
+++ dashboard/src/views/Dashboard.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { getAllCategories } from '../apis/Categories'
+import { getAllCategories, getCategoryProducts } from '../apis/Categories'
 
 export default function Dashboard() {
   const [categoryTiles, setCategoryTiles] = useState([])
+  const [phones, setPhones] = useState([])
   useEffect(() => {
     let categories = getAllCategories();
     categories.then(response => { console.log(response); setCategoryTiles(response) });
+    let smartphones = getCategoryProducts('smartphones');
+    smartphones.then(response => { console.log(response); setPhones(response.products) });
   }, [])
-  return <h1> {categoryTiles.map(c => <button>{c}</button>)} </h1>;
+  return <div>
+    <div>
+      {categoryTiles.map(c => <button>{c}</button>)}
+    </div>
+    <div>
+      {phones.map(el => <button>{el.images[0]}</button>)}
+    </div>
+  </div>
+    ;
 }
