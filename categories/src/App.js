8fbdf9c4c8d051f53080bef32152597e99fef535
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-08-08 19:38:03 +0530

    Added product images on category page

--- categories/src/App.js
+++ categories/src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getCategoryProducts } from './apis/Categories.js'
 
 const App = ({ routeParams }) => {
   const { category } = routeParams;
+  const [prodImgs, setProdImgs] = useState([])
+  useEffect(() => {
+    getCategoryProducts(category)
+      .then(res => res.products)
+      // .then(console.log)
+      .then(res => {
+        console.log(res)
+        res= res.map(el=>el.images[0])
+        console.log(res)
+        setProdImgs(res)
+      })
+
+    console.log(prods)
+    // products = products.map((el) => el.images[0])
+    // console.log(products)
+  }, [])
+  let prods = prodImgs?.map(el=><img src={el}/>)
   return (
     <div>
       <h1>
         Categories - {category}
       </h1>
+      {prods}
     </div>
   );
 };
