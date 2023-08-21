33601a1eaff4bde785af26f6ac3bffd69f81c72e
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-08-21 12:08:25 +0530

    Added handling for no cart items

--- container/src/components/Cart.js
+++ container/src/components/Cart.js
@@ -7,46 +7,52 @@ export default function Cart() {
   const user = "1234567890";
   useEffect(() => {
     let items = localStorage && localStorage.getItem("carts");
-    items = JSON.parse(items);
-    items = items[user];
-    console.log(items);
-    let orderTotal = 0,
-      qtyTotal = 0;
-    setProducts(
-      items.map((item) => {
-        const { title, price, image, id, quantity } = item;
-        orderTotal += price * quantity;
-        qtyTotal += parseInt(quantity);
-        return (
-          <a
-            href={`/product/${id}`}
-            style={{ color: "black", textDecoration: "none" }}
-          >
-            <div
-              style={{ display: "flex", flexDirection: "row", margin: "1rem" }}
+    if (items) {
+      items = JSON.parse(items);
+      items = items[user];
+      console.log(items);
+      let orderTotal = 0,
+        qtyTotal = 0;
+      setProducts(
+        items.map((item) => {
+          const { title, price, image, id, quantity } = item;
+          orderTotal += price * quantity;
+          qtyTotal += parseInt(quantity);
+          return (
+            <a
+              href={`/product/${id}`}
+              style={{ color: "black", textDecoration: "none" }}
             >
-              <img
-                src={image}
+              <div
                 style={{
-                  height: "8rem",
-                  width: "8rem",
-                  margin: "0 1rem 1rem 0",
+                  display: "flex",
+                  flexDirection: "row",
+                  margin: "1rem",
                 }}
-              />
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <h5>
-                  {title} ({quantity})
-                </h5>
-                <h3>Rs. {price}</h3>
-                {/* {price * quantity} */}
+              >
+                <img
+                  src={image}
+                  style={{
+                    height: "8rem",
+                    width: "8rem",
+                    margin: "0 1rem 1rem 0",
+                  }}
+                />
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <h5>
+                    {title} ({quantity})
+                  </h5>
+                  <h3>Rs. {price}</h3>
+                  {/* {price * quantity} */}
+                </div>
               </div>
-            </div>
-          </a>
-        );
-      })
-    );
-    setTotalPrice(orderTotal);
-    setTotalItems(qtyTotal);
+            </a>
+          );
+        })
+      );
+      setTotalPrice(orderTotal);
+      setTotalItems(qtyTotal);
+    }
   }, []);
   return (
     <div style={{ display: "flex", justifyContent: "center", margin: "4rem" }}>
@@ -57,21 +63,27 @@ export default function Cart() {
           alignItems: "center",
         }}
       >
-        <div>Subtotal: Rs.{totalPrice}</div>
-        <div>Items: {totalItems}</div>
-        {products}
-        <button
-          style={{
-            backgroundColor: "#FFA41C",
-            borderColor: "#FFA41C",
-            padding: "0.8rem 3rem",
-            borderRadius: "2rem",
-            fontSize: "1.2rem",
-            marginRight: "1rem",
-          }}
-        >
-          Buy Now
-        </button>
+        {products.length > 0 ? (
+          <React.Fragment>
+            <div>Subtotal: Rs.{totalPrice}</div>
+            <div>Items: {totalItems}</div>
+            {products}
+            <button
+              style={{
+                backgroundColor: "#FFA41C",
+                borderColor: "#FFA41C",
+                padding: "0.8rem 3rem",
+                borderRadius: "2rem",
+                fontSize: "1.2rem",
+                marginRight: "1rem",
+              }}
+            >
+              Buy Now
+            </button>
+          </React.Fragment>
+        ) : (
+          <h4>Your Cart is Empty</h4>
+        )}
       </div>
     </div>
   );
