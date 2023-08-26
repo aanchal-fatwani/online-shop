ec766e6d42bca8e4c5b7d93dd8be22c887ac72a3
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-08-26 15:17:47 +0530

    Added login screen

---
+++ container/src/components/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/login.css";
+
+const Login = () => {
+  return (
+    <React.Fragment>
+      <div className="card" id="card">
+        <h1>Sign In</h1>
+        <div className="form">
+          <label>Enter mobile phone number or email</label>
+          <input type="text" />
+          <button> Continue</button>
+        </div>
+        <a href="#" className="icon">
+          Need help?
+        </a>
+      </div>
+      <div className="breck">
+        <p>New here?</p>
+      </div>
+      <div className="btn">
+        <button> Create your new account</button>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Login;
