11ceb351714b018f38533d158fd323ae921a6652
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-08-27 18:09:29 +0530

    Added register screen

---
+++ container/src/components/Register.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "../styles/login.css";
+
+const Register = () => {
+  return (
+    <React.Fragment>
+      <div className="card card-reg" id="card">
+        <h1>Create Account</h1>
+        <div className="form">
+          <label>Your name</label>
+          <input type="text" />
+          <label>Mobile number</label>
+          <input type="text" />
+          <label>Email (optional)</label>
+          <input type="text" />
+          <label>Password</label>
+          <input type="password" />
+          <button> Continue </button>
+        </div>
+        <p>
+          Already have an account? &nbsp;
+          <a href="#" className="icon">
+            Sign in
+          </a>
+        </p>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Register;
