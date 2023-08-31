f7f4af5f250e06848d252e9827c0ca8b3eff1f1e
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-08-31 22:45:27 +0530

    Updated login page

--- container/src/components/Login.js
+++ container/src/components/Login.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+
+  function submitHandler(e) {
+    e.preventDefault();
+    if (!email) {
+      alert("Please enter your email for login!");
+      return;
+    }
+    setEmail("");
+  }
+
   return (
     <React.Fragment>
       <div className="card" id="card">
         <h1>Sign In</h1>
         <div className="form">
-          <label>Enter mobile phone number or email</label>
-          <input type="text" />
-          <button> Continue</button>
+          <label>Enter email</label>
+          <input type="text" onChange={(e) => setEmail(e.target.value)}/>
+          <button onClick={(e) => submitHandler(e)}> Continue </button>
         </div>
         <a href="/help" className="icon">
           Need help?
