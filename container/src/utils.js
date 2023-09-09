637ba061a8e4b1a5e945cd096d5d674829334172
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-09-10 02:46:44 +0530

    Added login utilities

---
+++ container/src/utils.js
@@ -0,0 +1,43 @@
+export function addNewUser(name, mobile, email, password) {
+  if (localStorage) {
+    let userDetails = {
+      name,
+      mobile,
+      email,
+      password,
+    };
+    let currentUsers = localStorage.getItem("allUsers");
+    if (currentUsers) currentUsers = JSON.parse(currentUsers);
+    else currentUsers = {};
+    currentUsers[mobile] = userDetails;
+
+    localStorage.setItem("allUsers", JSON.stringify(currentUsers));
+    alert("User added");
+    loginUser(mobile, true);
+  } else {
+    alert("Some issue occurred!");
+  }
+}
+
+export function loginUser(mobile, newUser = false) {
+  if (localStorage) {
+    let allUsers;
+    if (!newUser) {
+      allUsers = JSON.parse(localStorage["allUsers"]);
+    }
+    if (!allUsers[mobile]) {
+      alert("User does not exist.. Sign-up first or try again!");
+    } else {
+      localStorage.setItem("user", JSON.stringify(mobile));
+      alert("You have successfully logged in!");
+      window.location.href = "/";
+    }
+  } else {
+    alert("Some issue occurred!");
+  }
+}
+
+export function checkLoggedInState() {
+  if (localStorage && localStorage.user) return true;
+  return false;
+}
