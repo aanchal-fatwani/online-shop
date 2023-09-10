c4016b5da2b6cf8c3d10bb26e4eb1cef1d41f6c7
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-09-10 23:31:26 +0530

    Updated register page

--- container/src/components/Register.js
+++ container/src/components/Register.js
@@ -1,21 +1,61 @@
 import React, { useState } from "react";
 import "../styles/login.css";
+import { addNewUser } from "../utils.js";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function submitHandler(e) {
+    e.preventDefault();
+    if (!name || !mobile || !password) {
+      alert("Please enter required details!");
+      return;
+    }
+    addNewUser(name, mobile, email, password);
+    setName("");
+    setMobile("");
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <React.Fragment>
       <div className="card card-reg" id="card">
         <h1>Create Account</h1>
         <div className="form">
           <label>Your name</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <label>Mobile number</label>
-          <input type="text" />
+          <input
+            type="number"
+            value={mobile}
+            onChange={(e) => setMobile(e.target.value)}
+          />
           <label>Email (optional)</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label>Password</label>
-          <input type="password" />
-          <button style={{ cursor: "pointer" }}> Continue </button>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            style={{ cursor: "pointer" }}
+            onClick={(e) => submitHandler(e)}
+          >
+            Continue
+          </button>
         </div>
         <p>
           Already have an account? &nbsp;
