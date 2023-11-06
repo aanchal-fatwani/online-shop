c3ac8075f9042186849d13e79fc3abea9bf2d7c4
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-11-06 23:45:53 +0530

    Fixes

--- container/src/components/Register.js
+++ container/src/components/Register.js
@@ -39,6 +39,12 @@ const Register = () => {
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                submitHandler(e);
+              }
+            }}
           />
           <label>Mobile number</label>
           <input
@@ -46,18 +52,36 @@ const Register = () => {
             maxLength="10"
             value={mobile}
             onChange={(e) => setMobile(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                submitHandler(e);
+              }
+            }}
           />
           <label>Email (optional)</label>
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                submitHandler(e);
+              }
+            }}
           />
           <label>Password</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                submitHandler(e);
+              }
+            }}
           />
           <button
             style={{ cursor: "pointer" }}
