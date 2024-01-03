7e69d288c7012f820288f55e0b656ef6317d354e
aanchal-fatwani <fatwaniaanchal@gmail.com>
2024-01-03 23:33:24 +0530

    Added JS ianalyzer fixes

--- dashboard/src/apis/Categories.js
+++ dashboard/src/apis/Categories.js
@@ -1,6 +1,5 @@
 export function getAllCategories() {
-  return fetch("https://dummyjson.com/products/categories").then((res) =>
-    res.json()
+  return fetch("https://dummyjson.com/products/categories").then((res) => res.json()
   );
 }
 
